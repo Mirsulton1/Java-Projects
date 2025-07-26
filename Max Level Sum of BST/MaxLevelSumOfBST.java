657a123100bcd3b0252143a25e5853781c3efd5a
Mirsulton #1 <91003008+Mirsulton1@users.noreply.github.com>
2025-07-26 16:07:37 -0400

    Create MaxLevelSumOfBST.java

---
+++ Max Level Sum of BST/MaxLevelSumOfBST.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class TreeNode{
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int val){
+        this.val = val;
+    }
+}
+
+class MaxLevelSumOfBST {
+    public int maxLevelSum(TreeNode root) {
+        if (root == null) return 0;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        int maxSum = Integer.MIN_VALUE;
+        int maxLevel = 1;
+        int currentLevel = 1;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            int levelSum = 0;
+
+            // Sum all nodes at this level
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                levelSum += node.val;
+
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
+            }
+
+            // Compare and update max
+            if (levelSum > maxSum) {
+                maxSum = levelSum;
+                maxLevel = currentLevel;
+            }
+
+            currentLevel++;
+        }
+
+        return maxLevel;
+    }
+}
