1d374f437c01fa53c601721b388770009ba5b588
Mirsulton #1 <91003008+Mirsulton1@users.noreply.github.com>
2025-08-06 18:49:49 -0400

    Update SingleNumber.java

--- Single Number/SingleNumber.java
+++ Single Number/SingleNumber.java
@@ -1,22 +1,26 @@
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class SingleNumber {
-
-    public static int[] countBits(int n) {
-        int[] ans = new int[n + 1];
-        for (int i = 1; i <= n; i++) {
-            ans[i] = ans[i >> 1] + (i & 1);
+    
+    public static int singleNumber(int[] nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;  // XOR each number
         }
-        return ans;
+        return result;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter n: ");
-        int n = scanner.nextInt();
-        int[] result = countBits(n);
+        System.out.println("Enter space-separated integers (duplicates except one):");
+        String[] input = scanner.nextLine().split(" ");
+        int[] nums = new int[input.length];
 
-        System.out.println("Output: " + Arrays.toString(result));
+        for (int i = 0; i < input.length; i++) {
+            nums[i] = Integer.parseInt(input[i]);
+        }
+
+        int result = singleNumber(nums);
+        System.out.println("Single number: " + result);
     }
 }
